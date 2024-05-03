fcd5a04085e7aaf699622806989f7f5f7db41506
DivorcedLance <divorcedlance@gmail.com>
2024-05-03 16:52:41 -0500

    chore: Create component StudentSideBar

--- src/components/AsideButton.jsx
+++ src/components/AsideButton.jsx
@@ -1,8 +1,8 @@
 export function AsideButton({icon, label}) {
   return (
-    <button className="bg-[#189F4E]">
-      <img src={icon} alt="" />
-      <label>{label}</label>
+    <button className="bg-[#189F4E69] h-28 flex items-center pl-2 gap-3">
+      <img src={icon} alt="" className="h-28 w-28 p-3" />
+      <label className="font-[poppins] italic">{label}</label>
     </button>
   )
 }
--- src/pages/StudentPage.jsx
+++ src/pages/StudentPage.jsx
@@ -1,40 +1,14 @@
 import { Header } from '../components/Header'
-import notificationIcon from '../assets/notification_icon.svg'
-import default_avatar from '../assets/default_avatar.svg'
-import { AsideButton } from '../components/AsideButton'
+import { StudentSideBar } from '../components/StudentSideBar'
 
 export function StudentPage() {
   return (
     <>
       <Header />
-      <aside>
-        <nav className="">
-          <ul>
-            <AsideButton icon="/img/aside_icons/test_icon.svg" label="Test" />
-            <AsideButton
-              icon="/img/aside_icons/weekly_test_icon.svg"
-              label="Prueba Semanal"
-            />
-            <AsideButton
-              icon="/img/aside_icons/homework_icon.svg"
-              label="Tarea en casa"
-            />
-            <AsideButton
-              icon="/img/aside_icons/reinforcement_evaluation_icon.svg"
-              label="Evaluación de reforzamiento"
-            />
-          </ul>
-        </nav>
-        <div>
-          <img src={default_avatar} alt="" />
-          <div className="bg-[#969191] text-white font-bold text-center">
-            <h2>PATRICK GUZMAN</h2>
-            <p>Sin mensajes</p>
-            <img src={notificationIcon} alt="" />
-          </div>
-        </div>
-      </aside>
-      <main></main>
+      <StudentSideBar />
+      <main>
+        
+      </main>
     </>
   )
 }
