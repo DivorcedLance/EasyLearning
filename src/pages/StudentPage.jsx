d6afbda579d7db3444afb72d1559b2c3f2ef10f7
KiwiGin <bayeskiwigen@gmail.com>
2024-05-03 18:09:07 -0500

    chore: Add welcome_student_character image to StudentPage

--- src/pages/StudentPage.jsx
+++ src/pages/StudentPage.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/Header'
 import { StudentSideBar } from '../components/StudentSideBar'
 import { CardProfile } from '../components/CardProfile'
+import welcome_student_character from '../assets/welcome_student_character.svg'
 
 export function StudentPage() {
   return (
@@ -10,7 +11,7 @@ export function StudentPage() {
         <StudentSideBar />
         <main className='bg-cyan-100 w-full'>
             <CardProfile />
-            
+            <img src={welcome_student_character} alt="" />
           </main>
 
       </div>
