ec175df20f29bcc48a5553818798b7031784fa80
DivorcedLance <divorcedlance@gmail.com>
2024-06-01 17:30:56 -0500

    Add Word page to test CkEditor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { StudentPage } from './pages/StudentPage'
 import { TeacherPage } from './pages/TeacherPage'
 import { LoginPage } from './pages/LoginPage'
+import { WordPage } from './pages/WordPage'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage/>} />
+          <Route path="/word" element={<WordPage/>} />
           <Route path='/student' element={<StudentPage/>} />
           <Route path='/teacher' element={<TeacherPage/>} />
         </Routes>
